d3a3a0e6380608ccfee6b1affa18f23fcc109c81
新田光宏 <potimarimo@gmail.com>
2015-12-21 01:40:02 +0900

    151.以上記号の読み込みをSignReaderに移行

--- TinySQL/Solution/TinySQL/ExecuteSQL.cpp
+++ TinySQL/Solution/TinySQL/ExecuteSQL.cpp
@@ -288,6 +288,23 @@ public:
 	KeywordReader(const TokenKind kind, const string word);
 };
 
+//! 記号トークンを読み込む機能を提供します。
+class SignReader : public TokenReader
+{
+	Token sign; //!< 読み込む記号トークンと等しいトークンです。
+protected:
+	//! 実際にトークンを読み込みます。
+	//! @param [in] cursol 読み込み開始位置です。
+	//! @param [in] end SQL全体の終了位置です。
+	//! @return 切り出されたトークンです。読み込みが失敗した場合はnullptrを返します。
+	const shared_ptr<const Token> ReadCore(string::const_iterator &cursol, const string::const_iterator& end) const override;
+public:
+	//! KeywordReaderクラスの新しいインスタンスを初期化します。
+	//! @param [in] kind トークンの種類です。
+	//! @param [in] word キーワードの文字列です。
+	SignReader(const TokenKind kind, const string word);
+};
+
 //! ファイルに対して実行するSQLを表すクラスです。
 class SqlQuery
 {
@@ -530,6 +547,31 @@ const shared_ptr<const Token> KeywordReader::ReadCore(string::const_iterator &cu
 //! @param [in] word キーワードの文字列です。
 KeywordReader::KeywordReader(const TokenKind kind, const string word) : keyword(Token(kind, word)){}
 
+
+//! 実際にトークンを読み込みます。
+//! @param [in] cursol 読み込み開始位置です。
+//! @param [in] end SQL全体の終了位置です。
+//! @return 切り出されたトークンです。読み込みが失敗した場合はnullptrを返します。
+const shared_ptr<const Token> SignReader::ReadCore(string::const_iterator &cursol, const string::const_iterator &end) const
+{
+	auto result =
+		mismatch(sign.word.begin(), sign.word.end(), cursol,
+		[](const char keywordChar, const char sqlChar){return keywordChar == toupper(sqlChar); });
+
+	if (result.first == sign.word.end()){
+		cursol = result.second;
+		return make_shared<Token>(sign);
+	}
+	else{
+		return nullptr;
+	}
+}
+
+//! KeywordReaderクラスの新しいインスタンスを初期化します。
+//! @param [in] kind トークンの種類です。
+//! @param [in] word キーワードの文字列です。
+SignReader::SignReader(const TokenKind kind, const string word) : sign(Token(kind, word)){}
+
 //! 二つの文字列を、大文字小文字を区別せずに比較し、等しいかどうかです。
 //! @param [in] str1 比較される一つ目の文字列です。
 //! @param [in] str2 比較される二つ目の文字列です。
@@ -561,6 +603,7 @@ const shared_ptr<const vector<const Token>> SqlQuery::GetTokens(const string sql
 		make_shared<KeywordReader>(TokenKind::OR, "OR"),
 		make_shared<KeywordReader>(TokenKind::SELECT, "SELECT"),
 		make_shared<KeywordReader>(TokenKind::WHERE, "WHERE"),
+		make_shared<SignReader>(TokenKind::GREATER_THAN_OR_EQUAL, ">="),
 	};
 
 	auto backPoint = sql.begin(); // SQLをトークンに分割して読み込む時に戻るポイントを記録しておきます。
@@ -1438,7 +1481,6 @@ void SqlQuery::WriteCsv(const string outputFileName, const vector<const InputTab
 //! @param [in] sql 実行するSQLです。
 SqlQuery::SqlQuery(const string sql) :
 	signConditions({
-		{ TokenKind::GREATER_THAN_OR_EQUAL, ">=" },
 		{ TokenKind::LESS_THAN_OR_EQUAL, "<=" },
 		{ TokenKind::NOT_EQUAL, "<>" },
 		{ TokenKind::ASTERISK, "*" },
